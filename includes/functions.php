d20b0513f9a17317aee61639e77d4940b0965cd7
zag 2me <doowles2@hotmail.com>
2012-11-21 15:00:35 +0000

    removed useless functions from other projects

--- includes/functions.php
+++ includes/functions.php
@@ -2,15 +2,6 @@
 // PHP Functions //
 
 
-// Function to return the URL of the 1st Logo
-function searchLogo($search)
-{
-$ftvxml = simplexml_load_file('http://fanart.tv/webservice/artist/7a3d390582c28a09616bfab3d86fa3e6/' . $search . '/xml/');
-return $ftvxml->music->musiclogos->musiclogo[0]["url"];
-}
-
-
-
 //php function to check whether the url exists or not and validate it  
 function check_url($url)  
 {  
